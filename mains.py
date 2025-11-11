f794fa34cc46526357d6a1b1165e40761e9ca6a0
Shamalpk123 <spkumbhar1995@gmail.com>
2025-11-11 10:41:58 +0530

    mains.py

--- mains.py
+++ mains.py
@@ -1,28 +1,29 @@
 import streamlit as st
 import pandas as pd
-# st.write("hello welcome to streamlit")
-# st.title('Programming')
-# st.header("python world")
-# st.subheader("streamlit")
-# st.markdown('markdown')
-# st.code("""print("Welcome to Streamlit Class")""")
-# st.code("""for i in range(1,5,1): 
-# 	print("java")""")
-# dataset = pd.read_csv("titanic.csv")
-# st.dataframe(dataset)
+st.write("hello welcome to streamlit")
+st.title('Programming')
+st.header("python world")
+st.subheader("streamlit")
+st.markdown('markdown')
+st.code("""print("Welcome to Streamlit Class")""")
+st.code("""for i in range(1,5,1): 
+	print("java")""")
+dataset = pd.read_csv("titanic.csv")
+st.dataframe(dataset)
 
-# name = st.text_input("enter your name:")
-# fname = st.text_input("enter your father name:")
-# button = st.button("Done")
+name = st.text_input("enter your name:")
+fname = st.text_input("enter your father name:")
+button = st.button("Done")
 
-# classdata = st.selectbox("enter your class:", (1,2,3,4,5))
+classdata = st.selectbox("enter your class:", (1,2,3,4,5))
 
-# if button:
-#     st.markdown(f"""
-#     Name:{name}
-#     Father Name:{fname}
-#     class:{classdata} """)
+if button:
+    st.markdown(f"""
+    Name:{name}
+    Father Name:{fname}
+    class:{classdata} """)
 
 number = st.slider("pick a number", 0, 100)
+
 
 
